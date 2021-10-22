01e9a10e58e47205c1cbd5c60039801660368079
lesterpro <ytam10@my.centennialcollege.ca>
2021-10-22 23:20:17 +0800

    V0.2

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = 
 {
-    //"URI": "mongodb://localhost/assignments","Secret": 'SomeSecret'
+    //"URI": "mongodb://localhost/COMP299","Secret": 'SomeSecret'
     //"URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
     "URI": "mongodb+srv://yukit:lester299@comp229.sifge.mongodb.net/myFirstDatabase?retryWrites=true&w=majority","Secret": 'SomeSecret'
 }
